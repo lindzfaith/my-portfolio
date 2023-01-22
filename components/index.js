bf2aae9fbcf000df7ce954dbee6e71f48e612848
Lindsey <69260837+lindzfaith@users.noreply.github.com>
2023-01-21 20:01:41 -0500

    Uploading components needed

---
+++ components/index.js
@@ -0,0 +1,7 @@
+export { default as Project } from './Project.jsx';
+export { default as Header } from './Header.jsx';
+export { default as HeroBanner } from './HeroBanner.jsx';
+export {default as AboutMe} from './AboutMe';
+export {default as Cat} from './Cat';
+export {default as FixedCat} from './FixedCat';
+export {default as Skills} from './Skills';
