c8cc0b72feec69f08b19e52e0f974c067b7868ef
Lindsey Dunkley <lfdunkle@ncsu.edu>
2023-09-12 17:02:04 -0400

    Fixed about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,13 +8,16 @@ function AboutMe() {
             </div>
             <div className="row">
                 <h2>About Me</h2>
+                <div className="image-container">
+                    <img src={require("./cat-picture.jpeg")}/>
+                </div>
                 <ul>
-                <li>I'm a senior-level Computer Science student earning a Bachelor's in December this year.</li>
-                <li>I have a deaf kitten named Peach.</li>
-                <li>I started coding in 2019.</li>
-                <li>I'm a first-generation college student.</li>
-                <li>My favorite programming language is JavaScript, but I love working with Java.</li>
-                <li>When I'm not coding, I'm reading or playing games.</li>
+                    <li>I'm a senior-level Computer Science student earning a Bachelor's in December this year.</li>
+                    <li>I have a deaf kitten named Peach.</li>
+                    <li>I started coding in 2019.</li>
+                    <li>I'm a first-generation college student.</li>
+                    <li>My favorite programming language is JavaScript, but I love working with Java.</li>
+                    <li>When I'm not coding, I'm reading or playing games.</li>
                 </ul>
             </div>
         </div>
